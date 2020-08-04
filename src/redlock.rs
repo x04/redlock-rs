a73e62677977602b708d01fa6c0b46922530fdb0
RIP RAIDEN <admin@splay.dev>
2020-08-03 20:34:49 -0400

    async stuff, unlock as well

--- src/redlock.rs
+++ src/redlock.rs
@@ -1,14 +1,16 @@
+use async_trait::async_trait;
+
 use rand::{thread_rng, Rng};
 use redis::Value::Okay;
 use redis::{RedisResult, Value};
 
-use std::thread::sleep;
-use std::time::{Duration, Instant};
 use std::error::Error;
 use std::ops::DerefMut;
 
-const DEFAULT_RETRY_COUNT: u32 = 3;
-const DEFAULT_RETRY_DELAY: u32 = 200;
+use tokio::time::{Duration, Instant};
+
+const DEFAULT_RETRY_COUNT: u32 = 5;
+const DEFAULT_RETRY_DELAY: u32 = 250;
 const CLOCK_DRIFT_FACTOR: f32 = 0.01;
 const UNLOCK_SCRIPT: &str = r"if redis.call('get',KEYS[1]) == ARGV[1] then
                                 return redis.call('del',KEYS[1])
@@ -23,8 +25,6 @@ const UNLOCK_SCRIPT: &str = r"if redis.call('get',KEYS[1]) == ARGV[1] then
 pub struct RedLock {
     /// List of all Redis clients
     pub pool: r2d2::Pool<redis::Client>,
-    retry_count: u32,
-    retry_delay: u32,
 }
 
 pub struct Lock<'a> {
@@ -39,33 +39,34 @@ pub struct Lock<'a> {
     pub lock_manager: &'a RedLock,
 }
 
+#[async_trait]
+trait LockUnlock {
+    async fn unlock(&self) -> bool;
+}
+
+#[async_trait]
+impl LockUnlock for Lock<'_> {
+    async fn unlock(&self) -> bool {
+        self.lock_manager.unlock(self.resource.as_slice(), self.val.as_slice()).await
+    }
+}
+
+#[async_trait]
+trait UnlockLock {
+    async fn unlock(&self, resource: &[u8], val: &[u8]) -> bool;
+}
+
 impl RedLock {
-    /// Create a new lock manager instance, defined by the given Redis connection uris.
-    /// Quorum is defined to be N/2+1, with N being the number of given Redis instances.
-    ///
-    /// Sample URI: `"redis://127.0.0.1:6379"`
+    /// Create a new lock manager instance, defined by the given Redis connection pool.
     pub fn new(pool: r2d2::Pool<redis::Client>) -> RedLock {
         RedLock {
             pool,
-            retry_count: DEFAULT_RETRY_COUNT,
-            retry_delay: DEFAULT_RETRY_DELAY,
         }
     }
 
-    /// Get 20 random bytes.
-    pub fn get_unique_lock_id<RNG: rand::RngCore>(&self, rand: &mut RNG) -> Vec<u8> {
-        let mut randoms = Vec::with_capacity(20);
-        rand.fill_bytes(&mut randoms);
-        randoms
-    }
-
-    /// Set retry count and retry delay.
-    ///
-    /// Retry count defaults to `3`.
-    /// Retry delay defaults to `200`.
-    pub fn set_retry(&mut self, count: u32, delay: u32) {
-        self.retry_count = count;
-        self.retry_delay = delay;
+    /// Get UUID for lock ID.
+    pub fn get_unique_lock_id(&self) -> Vec<u8> {
+        uuid::Uuid::new_v4().as_bytes().to_vec()
     }
 
     fn lock_instance(
@@ -98,11 +99,19 @@ impl RedLock {
     ///
     /// If it fails. `None` is returned.
     /// A user should retry after a short wait time.
-    pub fn lock(&self, resource: &[u8], ttl: usize) -> Result<Option<Lock>, Box<dyn Error>> {
+    pub async fn lock(&self, resource: &[u8], ttl: usize, retry_count: Option<u32>, retry_delay: Option<u32>) -> Result<Option<Lock<'_>>, Box<dyn Error>> {
         let mut rng = thread_rng();
-        let val = self.get_unique_lock_id(&mut rng);
+        let val = self.get_unique_lock_id();
 
-        for _ in 0..self.retry_count {
+        let retry_count = match retry_count {
+            Some(count) => count,
+            None => DEFAULT_RETRY_COUNT,
+        };
+        let retry_delay = match retry_delay {
+            Some(delay) => delay,
+            None => DEFAULT_RETRY_DELAY,
+        };
+        for _ in 0..retry_count {
             let start_time = Instant::now();
             let locked = self.lock_instance(resource, &val, ttl);
 
@@ -121,16 +130,19 @@ impl RedLock {
                     validity_time,
                 }));
             } else {
-                self.unlock(resource, &val);
+                let _ = self.unlock(resource, &val);
             }
 
-            let n = rng.gen_range(0, self.retry_delay);
-            sleep(Duration::from_millis(u64::from(n)));
+            let n: u64 = rng.gen_range(0, retry_delay).into();
+            tokio::time::delay_for(Duration::from_millis(n)).await;
         }
         Ok(None)
     }
+}
 
-    fn unlock(&self, resource: &[u8], val: &[u8]) -> bool {
+#[async_trait]
+impl UnlockLock for RedLock {
+    async fn unlock(&self, resource: &[u8], val: &[u8]) -> bool {
         let mut con = match self.pool.get() {
             Err(_) => return false,
             Ok(val) => val,
